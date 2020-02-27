5c088ba4cce73ded7cdc330c03b41c2d3e77cc75
illia_dev <illya.posv.b@gmail.com>
2020-02-27 11:11:24 +0200

    update enums UserRole & Status

--- src/main/java/com/courses/management/course/Status.java
+++ src/main/java/com/courses/management/course/Status.java
@@ -1,4 +1,17 @@
 package com.courses.management.course;
 
 public enum Status {
+    OT_STARTED("OT_STARTED"),
+    IN_PROGRESS("IN_PROGRESS"),
+    FINISHED("FINISHED");
+
+    private String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
