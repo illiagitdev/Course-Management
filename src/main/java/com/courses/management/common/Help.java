f78fe7ad34249a58d8b97ff5b9f2044cbd954eda
illiagitdev <illya.posv.b@gmail.com>
2020-03-04 00:46:08 +0200

    logging and help comand

--- src/main/java/com/courses/management/common/Help.java
+++ src/main/java/com/courses/management/common/Help.java
@@ -1,6 +1,24 @@
 package com.courses.management.common;
 
-public class Help {
+public class Help implements Command{
+    private View view;
+
     public Help(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public String command() {
+        return "help";
+    }
+
+    @Override
+    public void process() {
+        view.write("--------------------------------");
+        view.write("-------------HELP---------------");
+        view.write("---------------------------------");
+        view.write("Command         |   Description--");
+        view.write("create_course   |   create course-");
+        view.write("");
     }
 }
--- src/main/java/com/courses/management/common/MainController.java
+++ src/main/java/com/courses/management/common/MainController.java
@@ -1,38 +1,41 @@
 package com.courses.management.common;
 
+import com.courses.management.course.CreateCourse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainController {
+    private static final Logger LOG = LogManager.getLogger(MainController.class);
     private View view;
+    private List<Command> commands;
 
     public MainController(View view) {
         this.view = view;
+        this.commands = Arrays.asList(
+                new CreateCourse(view),
+                new Help(view)
+        );
     }
 
     public void read(){
         view.write("Welcome!");
         while (true){
+            view.write("Enter command or 'help' for details!");
             String read = view.read();
             doCommand(read);
         }
     }
 
-    private void doCommand(String read) {
-        switch (read){
-            case "help":{
-                view.write("Help information!");
+    private void doCommand(String input) {
+        LOG.debug(String.format("doCommand: input=%s", input));
+        for (Command command: commands){
+            if (command.canProcess(input)){
+                command.process();
                 break;
-            }
-            case "create_course":{
-                createCourse();
-                break;
-            }
-            default:{
-                view.write("Enter the correct command!");
             }
         }
-    }
-
-    private void createCourse() {
-        CourseService service = new CourseService(view);
-        service.createCourse();
     }
 }
