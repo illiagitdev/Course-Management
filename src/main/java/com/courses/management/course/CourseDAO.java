3a0086185a0259f8cab6f77291861799c9bd27d1
illiagitdev <illya.posv.b@gmail.com>
2020-03-04 00:45:28 +0200

    add logging

--- src/main/java/com/courses/management/course/CourseDAO.java
+++ src/main/java/com/courses/management/course/CourseDAO.java
@@ -1,12 +1,15 @@
 package com.courses.management.course;
 
 import com.courses.management.common.DataAccessObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CourseDAO extends DataAccessObject<Course> {
+    private static final Logger LOG = LogManager.getLogger(CourseDAO.class);
     private static final String INSERT = "INSERT INTO course(title, status) VALUES(?, ?);";
 
     public CourseDAO(Connection connection) {
@@ -20,6 +23,7 @@ public class CourseDAO extends DataAccessObject<Course> {
             statement.setString(2, course.getCourseStatus().getCourseStatus());
             statement.execute();
         } catch (SQLException e) {
+            LOG.error(String.format("", ));
             e.printStackTrace();
         }
     }
