f2b023e67d980a1c8c604d537affe0000d6cdec6
illiagitdev <illya.posv.b@gmail.com>
2020-03-08 13:51:04 +0200

    add GetCourseById command

---
+++ src/main/java/com/courses/management/course/command/GetCourseById.java
@@ -0,0 +1,42 @@
+package com.courses.management.course.command;
+
+import com.courses.management.common.Command;
+import com.courses.management.common.DataAccessObject;
+import com.courses.management.common.View;
+import com.courses.management.course.Course;
+import com.courses.management.course.CourseDAOImpl;
+
+public class GetCourseById implements Command {
+    private final View view;
+    private DataAccessObject<Course> courseDAO;
+
+    public GetCourseById(View view) {
+        this.view = view;
+        courseDAO = new CourseDAOImpl();
+    }
+
+    @Override
+    public String command() {
+        return "course_by_id";
+    }
+
+    @Override
+    public void process() {
+        view.write("Enter a course id");
+        int id = validateNumber(view.read());
+        Course course = courseDAO.get(id);
+        view.write("Course\t\tcourse status");
+        view.write(String.format("%s\t\t%s", course.getTitle(), course.getCourseStatus()));
+        view.write(String.format("Course with id = %s retrieved", id));
+    }
+
+    private int validateNumber(String value) {
+        int res = 0;
+        try {
+            res = Integer.parseInt(value);
+        } catch (RuntimeException e) {
+            view.write("Input not a number!");
+        }
+        return res;
+    }
+}
