9ae00fee8bb6df7aaefaa44e3df1463d824ee606
illia_dev <illya.posv.b@gmail.com>
2020-03-11 16:16:33 +0200

    update: extend ...Dao interface with DataAccessObject

--- src/main/java/com/courses/management/solution/SolutionDao.java
+++ src/main/java/com/courses/management/solution/SolutionDao.java
@@ -1,8 +1,10 @@
 package com.courses.management.solution;
 
+import com.courses.management.common.DataAccessObject;
+
 import java.util.List;
 
-public interface SolutionDao {
+public interface SolutionDao extends DataAccessObject<Solutions> {
     List<Solutions> getAllByUser(int id);
     List<Solutions> getAllByHomework(int id);
     Solutions get(int userId, int homeworkId);
--- src/main/java/com/courses/management/user/UserDAO.java
+++ src/main/java/com/courses/management/user/UserDAO.java
@@ -1,7 +1,9 @@
 package com.courses.management.user;
 
+import com.courses.management.common.DataAccessObject;
+
 import java.util.List;
 
-public interface UserDAO {
+public interface UserDAO extends DataAccessObject<User> {
     List<User> getByCourse(int courseId);
 }
