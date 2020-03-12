139c2ac79c768ed768c5771bac31c868160b3490
illia_dev <illya.posv.b@gmail.com>
2020-03-12 17:38:45 +0200

    update to update user by id command

--- src/main/java/com/courses/management/user/command/UpdateUser.java
+++ src/main/java/com/courses/management/user/command/UpdateUser.java
@@ -2,9 +2,12 @@ package com.courses.management.user.command;
 
 import com.courses.management.common.Command;
 import com.courses.management.common.View;
-import com.courses.management.user.User;
-import com.courses.management.user.UserDAO;
-import com.courses.management.user.UserDAOImpl;
+import com.courses.management.course.Course;
+import com.courses.management.course.CourseDAO;
+import com.courses.management.course.CourseDAOImpl;
+import com.courses.management.user.*;
+
+import java.util.Optional;
 
 public class UpdateUser implements Command {
     private final View view;
@@ -24,19 +27,38 @@ public class UpdateUser implements Command {
     public void process() {
         view.write("Enter user ID for update");
         int id = validateNumber(view.read());
-        view.write("Enter updated user first name:");
-        String firstName = validate(view.read());
-        view.write("Enter updated user last name:");
-        String lastName = validate(view.read());
-        view.write("Enter updated user email:");
-        String email = validate(view.read());
-        User user = new User();
-        user.setId(id);
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setEmail(email);
-        userDAO.update(user);
-        view.write(String.format("User updated, id: %d", user.getId()));
+        User isExist = userDAO.get(id);
+        if (isExist != null) {
+            view.write("Enter updated user first name:");
+            String firstName = validate(view.read());
+            view.write("Enter updated user last name:");
+            String lastName = validate(view.read());
+            view.write("Enter updated user email:");
+            String email = validate(view.read());
+            view.write("Enter updated user status:");
+            String status = validate(view.read());
+            Optional<UserStatus> tmp = UserStatus.getUserStatus(status.toUpperCase());
+            UserStatus userStatus = tmp.isEmpty() ? isExist.getStatus() : tmp.get();
+            view.write("Enter updated user role:");
+            String role = validate(view.read());
+            Optional<UserRole> tmpRole = UserRole.getUserRole(role.toUpperCase());
+            UserRole userRole = tmpRole.isEmpty() ? isExist.getUserRole() : tmpRole.get();
+            view.write("Enter course title");
+            String title = validate(view.read());
+            Course course = (new CourseDAOImpl().get(title));
+            User user = new User();
+            user.setId(id);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setEmail(email);
+            user.setStatus(userStatus);
+            user.setUserRole(userRole);
+            user.setCourse(course);
+            userDAO.update(user);
+            view.write(String.format("User updated, id: %d", user.getId()));
+        } else {
+            view.write(String.format("User with id=%d not exist.", id));
+        }
     }
 
     private String validate(String value) {
