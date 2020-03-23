a21bba4f92f6bb27a26d3e5e5b71cd242faf2bce
illiagitdev <illya.posv.b@gmail.com>
2020-03-24 01:21:12 +0200

    Add CustomStream and integration test for CreateUser & Help

--- src/test/java/com/courses/management/IntegrationTest.java
+++ src/test/java/com/courses/management/IntegrationTest.java
@@ -1,28 +1,135 @@
 package com.courses.management;
 
-import com.courses.management.common.Command;
-import com.courses.management.common.DatabaseConnectorTest;
-import com.courses.management.common.View;
-import com.courses.management.common.command.util.InputString;
-import com.courses.management.user.UserDAO;
-import com.courses.management.user.UserDAOImpl;
-import com.courses.management.user.command.CreateUser;
-import com.courses.management.user.command.FindUser;
+import com.courses.management.common.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class IntegrationTest {
-    private DatabaseConnectorTest dbConnector = new DatabaseConnectorTest();
-    private View view = mock(View.class);
-    private UserDAO userDAO = new UserDAOImpl(dbConnector.getDataSource());
-    private Command createUserCommand = new CreateUser(view, userDAO);
-    private Command findUserCommand = new FindUser(view, userDAO);
+    private DatabaseConnectorTest dbConnector;
+    private MainController mainController;
+    private CustomImputStream in;
+    private ByteArrayOutputStream out;
+
+    @Before
+    public void setup() {
+        this.dbConnector = new DatabaseConnectorTest();
+        this.mainController = new MainController(new Console(), dbConnector.getDataSource());
+        this.in = new CustomImputStream();
+        this.out = new ByteArrayOutputStream();
+        System.setIn(in);
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        clearDatabase();
+    }
 
     @Test
-    public void testCreateUser() {
-        InputString inputString = new InputString("create_users|Illia|Bondarchuk|illia.bondarchuk@email.com");
-        createUserCommand.process(inputString);
-        findUserCommand.process(new InputString("find_user|illia.bondarchuk@email.com"));
+    public void testHelpCommand () {
+        //given
+        in.add("help");
+        in.add("exit");
+
+        //when
+        mainController.read();
+
+        //then
+        assertEquals("Welcome!\n" +
+                "Enter command or use 'help' to list all available commands!\n" +
+                "--------------------------------------------------\n" +
+                "-------------------HELP---------------------------------\n" +
+                "--------------------------------------------------\n" +
+                "\t| create_course|title\n" +
+                "\t|\t-> create course\n" +
+                "\n" +
+                "\t| find_course|title\n" +
+                "\t|\t-> find course by title\n" +
+                "\n" +
+                "\t| update_course_title|oldTitle|newTitle\n" +
+                "\t|\t-> update course by title. Title is a unique field.\n" +
+                "\n" +
+                "\t| update_course_status|title|status\n" +
+                "\t|\t-> update course by status. Possible values: NOT_STARTED, IN_PROGRESS, FINISHED, DELETED\n" +
+                "\n" +
+                "\t| show_courses\n" +
+                "\t|\t-> show all course\n" +
+                "\n" +
+                "\t| delete_course|title\n" +
+                "\t|\t-> move course to DELETED status\n" +
+                "\n" +
+                "\t| create_user|firstName|lastName|email\n" +
+                "\t|\t-> create user, user role will be set as NEWCOMER\n" +
+                "\t|\t-> and user status asNOT_ACTIVE\n" +
+                "\n" +
+                "\t| find_user|email\n" +
+                "\t|\t-> find user by email\n" +
+                "\n" +
+                "\t| delete_user_course|email\n" +
+                "\t|\t-> delete user course and set user to NOT_ACTIVE status\n" +
+                "\n" +
+                "\t| find_all_users_by_course_title|courseTitle\n" +
+                "\t|\t-> get all user by specified course\n" +
+                "\n" +
+                "\t| find_all_users_by_user_status|status\n" +
+                "\t|\t-> get all users by specified status [ACTIVE, NOT_ACTIVE]\n" +
+                "\n" +
+                "\t| update_user_course|userEmail|courseTitle\n" +
+                "\t|\t-> update users course by specified course title\n" +
+                "\n" +
+                "\t| update_user_email|oldEmail|newEmail\n" +
+                "\t|\t-> update users email\n" +
+                "\n" +
+                "\t| exit\n" +
+                "\t|\t-> exit application\n" +
+                "--------------------------------------------------\n" +
+                "Exit application\n", getData());
+    }
+
+    @Test
+    public void testCreateCourseWithCorrectInputParameters () {
+        //given
+        in.add("create_course|Title_Java");
+        in.add("find_course|Title_Java");
+        in.add("exit");
+
+        //when
+        mainController.read();
+
+        //then
+        assertEquals("Welcome!\n" +
+                "Enter command or use 'help' to list all available commands!\n" +
+                "Course created with title: Title_Java\n" +
+                "Course\n" +
+                "\t title = Title_Java\n" +
+                "\t status = NOT_STARTED\n" +
+                "Exit application\n", getData());
+    }
+
+    private String getData() {
+        try {
+            String result = new String(out.toByteArray(), "UTF-8").replaceAll("\r\n", "\n");
+            out.reset();
+            return result;
+        } catch (UnsupportedEncodingException e) {
+            return e.getMessage();
+        }
+    }
+
+    private void clearDatabase() throws SQLException {
+        try (Connection connection = dbConnector.getDataSource().getConnection();
+             Statement statement = connection.createStatement()) {
+
+        }
     }
 }
